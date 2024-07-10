49a29d29f66ef00ffd54350b558c1ca7260a36fe
ShahriarArefin <arfin17310@gmail.com>
2024-07-10 21:01:20 +0600

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+   return (
+      <div className="h-screen flex items-center text-center justify-center">
+         <h1 className="text-6xl text-slate-800">Hello World</h1>
+      </div>
+   );
+}
